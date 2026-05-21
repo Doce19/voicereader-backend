81814897943f20479fe86c17e3112e3b857ff231
Exaucé TOHOUN <developer3n@gmail.com>
2026-05-21 15:52:50 +0100

    Fix Railway env

--- app/main.py
+++ app/main.py
@@ -12,11 +12,7 @@ app = FastAPI(title="VoiceReader API", version="1.0.0")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[ 
-        "http://localhost:3000",
-        "https://voicereader-frontend.onrender.com"
-        "https://voicereader-frontend.vercel.app",
-        "*"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
